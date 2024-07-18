6dde1f9bb0c0e897f419227a268f83ed87156157
danielbosa <danielbosa95@gmail.com>
2024-07-18 23:00:25 +0200

    Getting elements from html

---
+++ script.js
@@ -0,0 +1,10 @@
+// # FASE DI PREPARAZIONE
+// Recupero gli elementi di interesse dalla pagina
+const grid = document.querySelector('.grid');
+const scoreCounter = document.querySelector('.score-counter');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+const endGameScreen = document.querySelector('.end-game-screen');
+const playAgainButton = document.querySelector('.play-again');
+const finalScore = document.querySelector('.final-score');
+const turboButton = document.querySelector('#turbo');
